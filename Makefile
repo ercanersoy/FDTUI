65470fc743ba5b08a34bdb1e63672138e54fe5da
Ercan Ersoy <ercanersoy@outlook.com>
2017-06-28 16:56:50 +0300

    Some addings and removings Add menu functions in main.cpp and UPX line in Makefile. Remove two menu items and one menu of bar in main.cpp.

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+
+
 all: dosshell.exe
 
 dosshell.exe: main.obj
 	wlink system dos op stack=4096 file main.obj,fdostui\lib\fdtuid16.lib name dosshell.exe
+    upx --8086 dosshell.exe
 
 main.obj: main.cpp
 	wpp -mm -0 -bt=DOS -d3 -i=fodstui\source\fdostui\include main.cpp
--- main.cpp
+++ main.cpp
@@ -4,31 +4,78 @@
 
 #include <fdostui.hpp>
 #include <stdio.h>
+#include <stdlib.h>
+
+// Bar
+
+window *bar = new window(0, 0, _video_cols, 3);
+menubar *bar_menus= new menubar(0, 0, _video_cols, 1);
+
+// File Manager
+
+window *file_manager = new window(5, 6, 68, 15);
+menubar *file_manager_menus= new menubar(0, 0, _video_cols, 1);
+
+// Menu Functions
+
+void application_file_manager(menuitem *, void *)
+{
+   file_manager->draw();
+   return;
+}
+
+void application_edit(menuitem *, void *)
+{
+   system("EDIT");
+   return;
+}
+
+void application_edlin(menuitem *, void *)
+{
+   system("EDLIN");
+   return;
+}
+
+void reboot(menuitem *, void *)
+{
+   wm_deinit();
+   system("FDAPM WARMBOOT");
+   return;
+}
+
+void poweroff(menuitem *, void *)
+{
+   wm_deinit();
+   system("FDAPM POWEROFF");
+   return;
+}
 
 // Bar Menus
 
+struct menuitem internal_applications_menu[] =
+{
+   {reinterpret_cast<unsigned char const*>("File Manager"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, application_file_manager, 0},
+   {0}
+};
+
 struct menuitem dos_applications_menu[] =
 {
-   {reinterpret_cast<unsigned char const*>("EDIT"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},
-   {reinterpret_cast<unsigned char const*>("EDLIN"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},
-   {reinterpret_cast<unsigned char const*>("FDSHIELD"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},
-   {reinterpret_cast<unsigned char const*>("DEFRAG"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},
+   {reinterpret_cast<unsigned char const*>("EDIT"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, application_edit, 0},
+   {reinterpret_cast<unsigned char const*>("EDLIN"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, application_edlin, 0},
    {0}
 };
 
 struct menuitem exit_menu[] =
 {
-   {reinterpret_cast<unsigned char const*>("Reboot"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},
-   {reinterpret_cast<unsigned char const*>("Power Off"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},
+   {reinterpret_cast<unsigned char const*>("Reboot"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, reboot, 0},
+   {reinterpret_cast<unsigned char const*>("Power Off"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, poweroff, 0},
    {0}
 };
 
 struct menuitembar menus_of_bar[] =
 {
-   {reinterpret_cast<unsigned char const*>("File Manager"), 0, SCAN_NONE, 0},
-   {reinterpret_cast<unsigned char const*>("Command Shell"), 0, SCAN_NONE, 0},
+   {reinterpret_cast<unsigned char const*>("Internal Apps"), 0, SCAN_NONE, internal_applications_menu},
    {reinterpret_cast<unsigned char const*>("DOS Apps"), 0, SCAN_NONE, dos_applications_menu},
-   {reinterpret_cast<unsigned char const*>("About"), 0, SCAN_NONE, 0},
    {reinterpret_cast<unsigned char const*>("Exit"), 0, SCAN_NONE, exit_menu},
    {0}
 };
@@ -94,30 +141,26 @@ int main(void)
 
    // Bar
 
-   window *bar = new window(0, 0, _video_cols, 3);
    wm_register_window(bar);
    bar->set_attributes(window::EMPTY);
-   
-   menubar *bar_menus= new menubar(0, 0, _video_cols, 1);
+
    bar_menus->set_menu(menus_of_bar);
-   
+
    bar->add(bar_menus);
-   
+
    // File Manager
-   
-   window *file_manager = new window(5, 6, 68, 15);
+
    wm_register_window(file_manager);
    file_manager->set_title((unsigned char *)"File Manager");
-   
-   menubar *file_manager_menus= new menubar(0, 0, _video_cols, 1);
+
    file_manager_menus->set_menu(menus_of_file_manager);
-   
+
    file_manager->add(file_manager_menus);
-   
+
    // Draw bar
    
-   wm_draw(bar);
-
+   wm_draw_widget(bar);
+   
    // Run Window Manager
 
    wm_run();
