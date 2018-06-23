574d383e0c7e9cc848887f58f42cdd6a17dee5e8
Ercan Ersoy <ercanersoy@ercanersoy.net>
2018-06-23 21:48:51 +0300

    Some updates
    Complete history feature, add go menu items disabling or enabling, add
    upper_directory_control() function, add dosshell.h that includes global
    variables, function prototypes and included header files information.

---
+++ dosshell.h
@@ -0,0 +1,195 @@
+/*********************************
+ * FreeDOS TUI Shell Header File *
+ *********************************/
+
+#include <direct.h>
+#include <dos.h>
+#include <fdostui.hpp>
+#include <mouse.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "config.h"
+#include "lang\en.h"
+#include "lang\tr.h"
+
+// Common Variables
+
+// Current directory string pointer variable
+char *current_directory = (char *)malloc(MAX_PATH_LENGTH + 1);
+// Show hidden file variable
+bool show_hidden_file = false;
+// Clipboard item name string pointer variable
+char *clipboard_item_name = (char *)calloc(256, 1);
+// Clipboard item path string pointer variable
+char *clipboard_item_path = (char *)calloc(MAX_PATH_LENGTH, 1);
+// Clipboard status variable
+unsigned char clipboard_status = 0;  // 0 for nothing, 1 for copying, 2 for cutting.
+// History path names string array variable
+char history_path_names[HISTORY_LENGTH][MAX_PATH_LENGTH + 1];
+// History index variable
+int history_index = 0;
+
+// Bar
+
+// Window of bar pointer variable
+window *bar = new window(0, 0, _video_cols, 3);
+// Menubar of bar pointer variable
+menubar *bar_menus = new menubar(0, 0, _video_cols, 1);
+
+// File Manager
+
+// Window of file manager pointer variable
+window *file_manager = new window(5, 6, 68, 17);
+// Menubar of file manager pointer variable
+menubar *file_manager_menus = new menubar(0, 0, _video_cols, 1);
+// Current directory label of file manager pointer variable
+label *current_directory_label = new label(1, 2, 65, 1);
+// Current drivers listbox of file manager pointer variable
+listbox *drivers = new listbox(1, 3, 65, 3);
+// Current directories and files of file manager pointer variable
+listbox *directories_and_files = new listbox(1, 6, 65, 9);
+
+// Function prototypes
+
+// Exit window manager function
+void exit_window_manager(menuitem *, void *);
+// Command run function
+void command_run(char const *);
+// Copy file function
+void copy_file(char *, char *);
+// Directory view function
+void directory_view(menuitem *, void *);
+// Upper directory control function
+void upper_directory_control(void);
+// Change current directory function
+void change_current_directory(char *, unsigned char);
+// Copy directory function
+void copy_directory(char *, char *);
+// Remove item function
+char remove_item(char *);
+// History back function
+void history_back(struct menuitem *, void *);
+// History foward function
+void history_foward(menuitem *, void *);
+// Change to upper directory function
+void change_to_upper_directory(menuitem *, void *);
+// Show file manager function
+void show_file_manager(menuitem *, void *);
+// Run application EDIT function
+void run_application_edit(menuitem *, void *);
+// Run application EDLIN function
+void run_application_edlin(menuitem *, void *);
+// Quit function
+void quit(menuitem *, void *);
+// Reboot function
+void reboot(menuitem *, void *);
+// Poweroff function
+void poweroff(menuitem *, void *);
+// Item open function
+void item_open(menuitem *, void *);
+// New directory function
+void new_directory(menuitem *, void *);
+// Exit file manager function
+void file_manager_exit(menuitem *, void *);
+// Cut function
+void cut_item(menuitem *, void *);
+// Copy function
+void copy_item(menuitem *, void *);
+// Paste Function
+void paste_item(menuitem *, void *);
+// Delete Function
+void delete_item(menuitem *, void *);
+// Show hidden files function
+void show_hidden_files(menuitem *, void *);
+
+// Bar Menus
+
+// Internal applications menu of bar
+struct menuitem internal_applications_menu[] =
+{
+   {reinterpret_cast<unsigned char const*>(STRING_FILE_MANAGER), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, show_file_manager, 0},  // File manager
+   {0}
+};
+
+// DOS applications menu of bar
+struct menuitem dos_applications_menu[] =
+{
+   {reinterpret_cast<unsigned char const*>("EDIT"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, run_application_edit, 0},  // Edit
+   {reinterpret_cast<unsigned char const*>("EDLIN"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, run_application_edlin, 0},  // Edlin
+   {0}
+};
+
+// Exit menu of bar
+struct menuitem exit_menu[] =
+{
+   {reinterpret_cast<unsigned char const*>(STRING_QUIT), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, quit, 0},  // Quit
+   {reinterpret_cast<unsigned char const*>(STRING_REBOOT_COMPUTER), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, reboot, 0},  // Reboot Computer
+   {reinterpret_cast<unsigned char const*>(STRING_POWEROFF_COMPUTER), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, poweroff, 0},  // Power Off Computer
+   {0}
+};
+
+// Menubar of bar
+struct menuitembar menus_of_bar[] =
+{
+   {reinterpret_cast<unsigned char const*>(STRING_INTERNAL_APPLICATIONS), 0, SCAN_NONE, internal_applications_menu},  // Internal Applications
+   {reinterpret_cast<unsigned char const*>(STRING_DOS_APPLICATIONS), 0, SCAN_NONE, dos_applications_menu},  // Dos Applications
+   {reinterpret_cast<unsigned char const*>(STRING_EXIT), 0, SCAN_NONE, exit_menu},  // Exit
+   {0}
+};
+
+// File Manager Menus
+
+// Fİle menu of file manager
+struct menuitem file_menu[] =
+{
+   {reinterpret_cast<unsigned char const*>(STRING_OPEN), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, item_open, 0},  // Open
+   {reinterpret_cast<unsigned char const*>(STRING_NEW_DIRECTORY), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, new_directory, 0},  // New directory
+   {reinterpret_cast<unsigned char const*>(STRING_EXIT), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, file_manager_exit, 0},  // Exit
+   {0}
+};
+
+// Edit menu of file manager
+struct menuitem edit_menu[] =
+{
+   {reinterpret_cast<unsigned char const*>(STRING_CUT), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, cut_item, 0},  // Cut
+   {reinterpret_cast<unsigned char const*>(STRING_COPY), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, copy_item, 0},  // Copy
+   {reinterpret_cast<unsigned char const*>(STRING_PASTE), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, paste_item, 0},  // Paste
+   {reinterpret_cast<unsigned char const*>(STRING_DELETE), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, delete_item, 0},  // Delete
+   {0}
+};
+
+// View menu of file manager
+struct menuitem view_menu[] =
+{
+   {reinterpret_cast<unsigned char const*>(STRING_REFRESH), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, directory_view, 0},  // Refresh
+   {reinterpret_cast<unsigned char const*>(STRING_SHOW_HIDDEN_FILES), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_CHECKBOX | MENUITEM_SEPERATOR, show_hidden_files, 0},  // Show hidden files
+   {reinterpret_cast<unsigned char const*>(STRING_SORT), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE,  MENUITEM_SUBMENU, 0, 0},  // Sort
+   {reinterpret_cast<unsigned char const*>(STRING_BY_NAME), MENUITEM_MNEMONIC_NONE, 0,  SCAN_NONE, 0, 0, 0},  // By name
+   {reinterpret_cast<unsigned char const*>(STRING_BY_SIZE), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},  // By size
+   {reinterpret_cast<unsigned char const*>(STRING_BY_TYPE), MENUITEM_MNEMONIC_NONE, 0,  SCAN_NONE, 0, 0, 0},  // By type
+   {reinterpret_cast<unsigned char const*>(STRING_BY_DATE), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},  // By date
+   {0},
+   {0}
+};
+
+// Go menu of file manager
+struct menuitem go_menu[] =
+{
+   {reinterpret_cast<unsigned char const*>(STRING_BACK), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_DISABLED, history_back, 0},  // Back
+   {reinterpret_cast<unsigned char const*>(STRING_FOWARD), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_DISABLED, history_foward, 0},  // Foward
+   {reinterpret_cast<unsigned char const*>(STRING_UP), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_DISABLED, change_to_upper_directory, 0},  // Up
+   {0}
+};
+
+// Menubar of file manager
+struct menuitembar menus_of_file_manager[] =
+{
+   {reinterpret_cast<unsigned char const*>(STRING_FILE), 0, SCAN_NONE, file_menu},  // File
+   {reinterpret_cast<unsigned char const*>(STRING_EDIT), 0, SCAN_NONE, edit_menu},  // Edit
+   {reinterpret_cast<unsigned char const*>(STRING_VIEW), 0, SCAN_NONE, view_menu},  // View
+   {reinterpret_cast<unsigned char const*>(STRING_GO), 0, SCAN_NONE, go_menu},  // Go
+   {0}
+};
--- main.cpp
+++ main.cpp
@@ -2,55 +2,7 @@
  * FreeDOS TUI Shell Source File *
  *********************************/
 
-#include <direct.h>
-#include <dos.h>
-#include <fdostui.hpp>
-#include <mouse.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include "config.h"
-#include "lang\en.h"
-#include "lang\tr.h"
-
-// Common Variables
-
-// Current directory string pointer variable
-char *current_directory = (char *)malloc(MAX_PATH_LENGTH + 1);
-// Show hidden file variable
-bool show_hidden_file = false;
-// Clipboard item name string pointer variable
-char *clipboard_item_name = (char *)calloc(256, 1);
-// Clipboard item path string pointer variable
-char *clipboard_item_path = (char *)calloc(MAX_PATH_LENGTH, 1);
-// Clipboard status variable
-unsigned char clipboard_status = 0;  // 0 for nothing, 1 for copying, 2 for cutting.
-// History path names string array variable
-char history_path_names[HISTORY_LENGTH][MAX_PATH_LENGTH + 1];
-// History index variable
-int history_index = 0;
-
-// Bar
-
-// Window of bar pointer variable
-window *bar = new window(0, 0, _video_cols, 3);
-// Menubar of bar pointer variable
-menubar *bar_menus = new menubar(0, 0, _video_cols, 1);
-
-// File Manager
-
-// Window of file manager pointer variable
-window *file_manager = new window(5, 6, 68, 17);
-// Menubar of file manager pointer variable
-menubar *file_manager_menus = new menubar(0, 0, _video_cols, 1);
-// Current directory label of file manager pointer variable
-label *current_directory_label = new label(1, 2, 65, 1);
-// Current drivers listbox of file manager pointer variable
-listbox *drivers = new listbox(1, 3, 65, 3);
-// Current directories and files of file manager pointer variable
-listbox *directories_and_files = new listbox(1, 6, 65, 9);
+#include "dosshell.h"
 
 // Common Functions
 
@@ -118,6 +70,102 @@ void directory_view(menuitem *, void *)
       // Add item on the directories and files listbox
       directories_and_files->add(reinterpret_cast<unsigned char const*>(element->d_name));
    }
+}
+
+// Upper directory control function
+void upper_directory_control(void)
+{
+   if(strlen(current_directory) == 3)  // If directory exists
+   {
+      // Up directory menu disabled
+      go_menu[2].m_flags |= MENUITEM_DISABLED;
+   }
+   else  // If not directory exists
+   {
+      // Up directory menu enabled
+      go_menu[2].m_flags = !(go_menu[2].m_flags ^ MENUITEM_DISABLED);
+   }
+}
+
+// Change current directory function
+void change_current_directory(char *directory, unsigned char status)
+{
+   if(status == 0)  // If status is adding new history item
+   {
+      if(directory != 0)  // If directory pointer is not null
+      {
+         // Change Directory
+         chdir(directory);
+      }
+
+      if(history_index < HISTORY_LENGTH - 1)  // If history index is less than history length minus 1
+      {
+         // History index increase
+         history_index++;
+         // History save
+         strcpy(history_path_names[history_index], getcwd(current_directory, PATH_MAX + 1));
+
+         for(int i = history_index + 1; i < HISTORY_LENGTH - 1; i++)
+         {
+            history_path_names[i][0] = '\0';
+         }
+      }
+      else  // If history index is equals history length - 1
+      {
+         for(int i = 0; i < HISTORY_LENGTH - 1; i++)
+         {
+            // History shifting
+            strcpy(history_path_names[i], history_path_names[i + 1]);
+         }
+
+         // History save
+         strcpy(history_path_names[history_index], getcwd(current_directory, PATH_MAX + 1));
+      }
+   }
+   else if(status == 1)  // If status is changing previous history item
+   {
+      // Decrase History
+      history_index--;
+
+      // Change Directory
+      chdir(history_path_names[history_index]);
+   }
+   else if(status == 2)  // If status is changing previous history item
+   {
+      // Increase History
+      history_index++;
+
+      // Change Directory
+      chdir(history_path_names[history_index]);
+   }
+
+   // Upper directory control
+   upper_directory_control();
+
+   if(history_index == 0)  // If history index equals 0
+   {
+      // Back menu item disabled
+      go_menu[0].m_flags |= MENUITEM_DISABLED;
+   }
+   else
+   {
+      // Back menu item enabled
+      go_menu[0].m_flags = !(go_menu[0].m_flags ^ MENUITEM_DISABLED);
+   }
+
+   if((history_index == HISTORY_LENGTH - 1) || history_path_names[history_index + 1][0] == '\0')  // If history index equals history length minus 1 or next history item equals 0
+   {
+      // Foward menu item disabled
+      go_menu[1].m_flags |= MENUITEM_DISABLED;
+   }
+   else
+   {
+      // Foward menu item disabled
+      go_menu[1].m_flags = !(go_menu[1].m_flags ^ MENUITEM_DISABLED);
+   }
+
+   // Refresh directory
+   directory_view(0, 0);
 }
 
 // Copy file function
@@ -280,25 +328,24 @@ char remove_item(char *removing_item_path)
 }
 
 // History back function
-void history_back(menuitem *, void *)
+void history_back(struct menuitem *menu_item, void *)
 {
-
+   // Change current directory to back history item
+   change_current_directory(NULL, 1);
 }
 
 // History foward function
 void history_foward(menuitem *, void *)
 {
-
+   // Change current directory to foward history item
+   change_current_directory(0, 2);
 }
 
 // Change to upper directory function
-void change_to_upper_directory(menuitem *, void *)
+void change_to_upper_directory(struct menuitem *menu_item, void *)
 {
-   // Change current directory to  upper directory
-   chdir("..");
-
-   // Refresh directory
-   directory_view(0, 0);
+   // Change current directory to upper directory
+   change_current_directory("..", 0);
 }
 
 // Bar Menu Functions
@@ -363,43 +410,9 @@ void poweroff(menuitem *, void *)
    command_run("FDAPM POWEROFF");
 }
 
-// Bar Menus
-
-// Internal applications menu of bar
-struct menuitem internal_applications_menu[] =
-{
-   {reinterpret_cast<unsigned char const*>(STRING_FILE_MANAGER), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, show_file_manager, 0},  // File manager
-   {0}
-};
-
-// DOS applications menu of bar
-struct menuitem dos_applications_menu[] =
-{
-   {reinterpret_cast<unsigned char const*>("EDIT"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, run_application_edit, 0},  // Edit
-   {reinterpret_cast<unsigned char const*>("EDLIN"), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, run_application_edlin, 0},  // Edlin
-   {0}
-};
-
-// Exit menu of bar
-struct menuitem exit_menu[] =
-{
-   {reinterpret_cast<unsigned char const*>(STRING_QUIT), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, quit, 0},  // Quit
-   {reinterpret_cast<unsigned char const*>(STRING_REBOOT_COMPUTER), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, reboot, 0},  // Reboot Computer
-   {reinterpret_cast<unsigned char const*>(STRING_POWEROFF_COMPUTER), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, poweroff, 0},  // Power Off Computer
-   {0}
-};
-
-// Menubar of bar
-struct menuitembar menus_of_bar[] =
-{
-   {reinterpret_cast<unsigned char const*>(STRING_INTERNAL_APPLICATIONS), 0, SCAN_NONE, internal_applications_menu},  // Internal Applications
-   {reinterpret_cast<unsigned char const*>(STRING_DOS_APPLICATIONS), 0, SCAN_NONE, dos_applications_menu},  // Dos Applications
-   {reinterpret_cast<unsigned char const*>(STRING_EXIT), 0, SCAN_NONE, exit_menu},  // Exit
-   {0}
-};
-
 // Menu Function Menus
 
+// Item open function
 void item_open(menuitem *, void *)
 {
    // Get item name
@@ -413,10 +426,7 @@ void item_open(menuitem *, void *)
    if(S_ISDIR(status.st_mode))  // If item is dir
    {
       // Change directory
-      chdir(item_name);
-
-       // Refresh directories
-      directory_view(0, 0);
+      change_current_directory(item_name, 0);
    }
    else  // If item is file
    {
@@ -425,6 +435,7 @@ void item_open(menuitem *, void *)
    }
 }
 
+// New directory function
 void new_directory(menuitem *, void *)
 {
    // Make new directory of specified name
@@ -432,6 +443,13 @@ void new_directory(menuitem *, void *)
 
    // Refresh directories
    directory_view(0, 0);
+}
+
+// Exit file manager function
+void file_manager_exit(menuitem *, void *)
+{
+   // Hide file manager window
+   file_manager->set_hidden();
 }
 
 // Cut function
@@ -520,16 +538,8 @@ void delete_item(menuitem *, void *)
    directory_view(0, 0);
 }
 
-// Exit file manager function
-void file_manager_exit(menuitem *, void *)
-{
-   // Hide file manager window
-   file_manager->set_hidden();
-}
-
-
 // Show hidden files function
-void show_hidden_files(struct menuitem *menu_item, void *)
+void show_hidden_files(menuitem *menu_item, void *)
 {
    if(menu_item->m_flags & MENUITEM_VALUE)  // If checked
    {
@@ -543,60 +553,6 @@ void show_hidden_files(struct menuitem *menu_item, void *)
    // Refresh directories
    directory_view(0, 0);  
 }
-
-// File Manager Menus
-
-// Fİle menu of file manager
-struct menuitem file_menu[] =
-{
-   {reinterpret_cast<unsigned char const*>(STRING_OPEN), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, item_open, 0},  // Open
-   {reinterpret_cast<unsigned char const*>(STRING_NEW_DIRECTORY), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, new_directory, 0},  // New directory
-   {reinterpret_cast<unsigned char const*>(STRING_EXIT), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, file_manager_exit, 0},  // Exit
-   {0}
-};
-
-// Edit menu of file manager
-struct menuitem edit_menu[] =
-{
-   {reinterpret_cast<unsigned char const*>(STRING_CUT), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, cut_item, 0},  // Cut
-   {reinterpret_cast<unsigned char const*>(STRING_COPY), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, copy_item, 0},  // Copy
-   {reinterpret_cast<unsigned char const*>(STRING_PASTE), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, paste_item, 0},  // Paste
-   {reinterpret_cast<unsigned char const*>(STRING_DELETE), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, delete_item, 0},  // Delete
-   {0}
-};
-
-// View menu of file manager
-struct menuitem view_menu[] =
-{
-   {reinterpret_cast<unsigned char const*>(STRING_REFRESH), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_SEPERATOR, directory_view, 0},  // Refresh
-   {reinterpret_cast<unsigned char const*>(STRING_SHOW_HIDDEN_FILES), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, MENUITEM_CHECKBOX | MENUITEM_SEPERATOR, show_hidden_files, 0},  // Show hidden files
-   {reinterpret_cast<unsigned char const*>(STRING_SORT), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE,  MENUITEM_SUBMENU, 0, 0},  // Sort
-   {reinterpret_cast<unsigned char const*>(STRING_BY_NAME), MENUITEM_MNEMONIC_NONE, 0,  SCAN_NONE, 0, 0, 0},  // By name
-   {reinterpret_cast<unsigned char const*>(STRING_BY_SIZE), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},  // By size
-   {reinterpret_cast<unsigned char const*>(STRING_BY_TYPE), MENUITEM_MNEMONIC_NONE, 0,  SCAN_NONE, 0, 0, 0},  // By type
-   {reinterpret_cast<unsigned char const*>(STRING_BY_DATE), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, 0, 0},  // By date
-   {0},
-   {0}
-};
-
-// Go menu of file manager
-struct menuitem go_menu[] =
-{
-   {reinterpret_cast<unsigned char const*>(STRING_BACK), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, history_back, 0},  // Back
-   {reinterpret_cast<unsigned char const*>(STRING_FOWARD), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, history_foward, 0},  // Foward
-   {reinterpret_cast<unsigned char const*>(STRING_UP), MENUITEM_MNEMONIC_NONE, 0, SCAN_NONE, 0, change_to_upper_directory, 0},  // Up
-   {0}
-};
-
-// Menubar of file manager
-struct menuitembar menus_of_file_manager[] =
-{
-   {reinterpret_cast<unsigned char const*>(STRING_FILE), 0, SCAN_NONE, file_menu},  // File
-   {reinterpret_cast<unsigned char const*>(STRING_EDIT), 0, SCAN_NONE, edit_menu},  // Edit
-   {reinterpret_cast<unsigned char const*>(STRING_VIEW), 0, SCAN_NONE, view_menu},  // View
-   {reinterpret_cast<unsigned char const*>(STRING_GO), 0, SCAN_NONE, go_menu},  // Go
-   {0}
-};
 
 // Main Function
 
@@ -612,9 +568,14 @@ int main(void)
    }
 
    // Current Directory
-   
+
+   // Current directory variable allocate
    current_directory = (char *) malloc(PATH_MAX + 1);
+   // Get current directory
    getcwd(current_directory, PATH_MAX + 1);
+
+   // Upper directory control
+   upper_directory_control();
 
    // Bar
 
@@ -629,6 +590,9 @@ int main(void)
    bar->add(bar_menus);
 
    // File Manager
+
+   // History initalizing
+   strcpy(history_path_names[0], current_directory);
 
    // Register file manager window
    wm_register_window(file_manager);
